f57ba4a2e869ec8efa530b30f1cdb54a6ab3a31c
RandomAdversary <randomadversary@gmail.com>
2016-04-08 18:11:48 +0200

    Initial commit
    Initial commit

--- js/raspored.js
+++ js/raspored.js
@@ -16,10 +16,8 @@
       else
          {
         // 	$(".loaderStatus").show();
+        $(".loaderStatus").hide();
          	courses = [];
-         //	setAutocomplete();
-         var worker = new Worker('autoComplete.js');
-         worker.postMessage();
          }
 
   });
@@ -28,9 +26,7 @@ function addCourses()
 {
 	$("#addbtn").hide();
 	$(".loaderStatus").show();
-	var worker = new Worker('autoComplete.js');
-    worker.postMessage();
-	//$(".addCourse").show();
+	setAutocomplete();
 }
   function sleep(milliseconds) {
       var start = new Date().getTime();
@@ -130,3 +126,35 @@ function loadByInstructor()
       });
   };
 
+  function setAutocomplete() {
+
+      //Profesori & Предмети
+      $.get('https://jsonp.afeld.me/?url=http://raspored.finki.ukim.mk/services/professors', function(data) {
+          instructorsWithID = data;
+          for (i = 0; i < data.length; i++) {
+              instructors.push(data[i].Name);
+              $.get('https://jsonp.afeld.me/?url=http://raspored.finki.ukim.mk/services/ScheduleByprofessor?value=' + data[i].Id, function(cData) {
+                  for (c = 0; c < cData.length; c++) {
+                      var title = cData[c].title.split("- Просторија:")[0];
+                     // console.log(title);
+                      //	console.log(allCourses.indexOf(title));
+                      if (allCourses.indexOf(title) == -1) {
+                          allCourses.push(title);
+                      }
+
+                  }
+                   $(".loaderStatus").html("Loading " + i + " out of " + data.length);
+              });
+             
+              sleep(320);
+            $(".loaderStatus").hide();
+      		$(".addCourse").show();
+          }
+      });
+      var input = document.getElementById("Професор");
+      var awesomplete = new Awesomplete(input);
+      awesomplete.list = instructors;
+      input = document.getElementById("Предмет");
+      awesomplete = new Awesomplete(input);
+      awesomplete.list = allCourses;
+  }
